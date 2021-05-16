201634f421b6a633e0dd2209c3f6991275fdfcb0
Majkie <majkie.tros@gmail.com>
2021-05-16 20:55:25 +0200

    Latte mix function for versioning and remove hot (wip)

---
+++ app/Components/Functions/MixFunction.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace App\Components\Functions;
+
+
+use Nette\Utils\FileSystem;
+use Nette\Utils\Strings;
+
+class MixFunction
+{
+
+    /**
+     * @param $path
+     * @param string $manifestDirectory
+     * @return mixed
+     * @throws \Exception
+     */
+    public static function mix($path, string $manifestDirectory = ''): mixed
+    {
+        static $manifests = [];
+
+        if (!Strings::startsWith($path, '/')) {
+            $path = "/$path";
+        }
+
+        if ($manifestDirectory && !Strings::startsWith($manifestDirectory, '/')) {
+            $manifestDirectory = "$manifestDirectory/";
+        }
+
+        $manifestPath = $manifestDirectory.'mix-manifest.json';
+
+        if (!isset($manifests[$manifestPath])) {
+            if (!is_file($manifestPath)) {
+                throw new \Exception('The mix manifest does not exist');
+            }
+
+            $manifests[$manifestPath] = json_decode(FileSystem::read($manifestPath), true);
+        }
+
+        $manifest = $manifests[$manifestPath];
+
+        if (!isset($manifest[$path])) {
+            throw new \Exception("Unable to locate Mix file: $path.");
+        }
+
+        return $manifestDirectory.$manifest[$path];
+    }
+
+}
